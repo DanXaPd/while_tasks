824b50bc79b7a1ee7db9b80496be363faec4c85e
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-02-06 19:08:59 +0200

    while_task

---
+++ main.js
@@ -0,0 +1,39 @@
+// Number_1
+/*let s = 1
+let a = 0
+let n = +prompt("Введите конечное нечетное число")
+while(s <= n){
+    a+=s
+    s+=2
+}
+alert(a)*/
+
+// Number 2
+/*let k = +prompt("Введите факториал числа ")
+let a = 1
+let mull = 1
+while (a <= k) {
+    mull*=a
+    a++
+}
+alert(mull)*/
+
+// Number 3
+/*let n = +prompt("Введите количество слагаемых")
+let a = 1
+let b = 1/2
+let sum = 0
+while (a <= n) {
+    sum+=b
+    b/=2
+    a++
+}
+alert(sum)*/
+
+//Number 4
+/*let a = +prompt("Введите целое положительное число")
+let b = 1;
+while((a /= 10) > 1){
+    b++
+}
+alert(b)*/
